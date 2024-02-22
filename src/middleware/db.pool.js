6640cb63632f1b2337ef9caf202014482e62400d
wh.lee <wh.lee@causeworks.co.kr>
2024-02-22 11:39:20 +0900

    FIX. 서버 시작시 db풀 한개만 생성

--- src/middleware/db.pool.js
+++ src/middleware/db.pool.js
@@ -16,67 +16,36 @@ switch(process.env.NODE_ENV) {
         break;
 }
 const envConfig = require(envConfigFile);
-
+let myAppPool = null;
 appPool = function () {
-    return mysql.createPool({
-        connectionLimit: `${envConfig.DB.APP_DB.connectionLimit}`,
-        host: `${envConfig.DB.APP_DB.host}`,
-        user: `${envConfig.DB.APP_DB.user}`,
-        password: `${envConfig.DB.APP_DB.password}`,
-        database: `${envConfig.DB.APP_DB.database}`,
-        port: `${envConfig.DB.APP_DB.port}`
-    });
+
+    if(!myAppPool){
+        console.log("크리에이트풀")
+        myAppPool = mysql.createPool({
+            connectionLimit: `${envConfig.DB.APP_DB.connectionLimit}`,
+            host: `${envConfig.DB.APP_DB.host}`,
+            user: `${envConfig.DB.APP_DB.user}`,
+            password: `${envConfig.DB.APP_DB.password}`,
+            database: `${envConfig.DB.APP_DB.database}`,
+            port: `${envConfig.DB.APP_DB.port}`
+        });
+        return myAppPool
+    }else{
+
+        return myAppPool
+    }
+
+    // return myAppPool = mysql.createPool({
+    //     connectionLimit: `${envConfig.DB.APP_DB.connectionLimit}`,
+    //     host: `${envConfig.DB.APP_DB.host}`,
+    //     user: `${envConfig.DB.APP_DB.user}`,
+    //     password: `${envConfig.DB.APP_DB.password}`,
+    //     database: `${envConfig.DB.APP_DB.database}`,
+    //     port: `${envConfig.DB.APP_DB.port}`
+    // });
 };
 
-infoPool = function () {
-    return mysql.createPool({
-        connectionLimit: `${envConfig.DB.INFO_DB.connectionLimit}`,
-        host: `${envConfig.DB.INFO_DB.host}`,
-        user: `${envConfig.DB.INFO_DB.user}`,
-        password: `${envConfig.DB.INFO_DB.password}`,
-        database: `${envConfig.DB.INFO_DB.database}`,
-        port: `${envConfig.DB.INFO_DB.port}`
-    });
-};
-
-pushPool = function () {
-    return mysql.createPool({
-        connectionLimit: `${envConfig.DB.PUSH_DB.connectionLimit}`,
-        host: `${envConfig.DB.PUSH_DB.host}`,
-        user: `${envConfig.DB.PUSH_DB.user}`,
-        password: `${envConfig.DB.PUSH_DB.password}`,
-        database: `${envConfig.DB.PUSH_DB.database}`,
-        port: `${envConfig.DB.PUSH_DB.port}`
-    });
-};
-
-logPool = function () {
-    return mysql.createPool({
-        connectionLimit: `${envConfig.DB.LOG_DB.connectionLimit}`,
-        host: `${envConfig.DB.LOG_DB.host}`,
-        user: `${envConfig.DB.LOG_DB.user}`,
-        password: `${envConfig.DB.LOG_DB.password}`,
-        database: `${envConfig.DB.LOG_DB.database}`,
-        port: `${envConfig.DB.LOG_DB.port}`
-    });
-};
-
-// 개인 암호키 DB 추가     @ALEX - 2021.12.15
-cipherPool = function () {
-    return mysql.createPool({
-        connectionLimit: `${envConfig.DB.CIPHER_DB.connectionLimit}`,
-        host: `${envConfig.DB.CIPHER_DB.host}`,
-        user: `${envConfig.DB.CIPHER_DB.user}`,
-        password: `${envConfig.DB.CIPHER_DB.password}`,
-        database: `${envConfig.DB.CIPHER_DB.database}`,
-        port: `${envConfig.DB.CIPHER_DB.port}`
-    });
-};
 
 module.exports = {
     appPool,
-    infoPool,
-    pushPool,
-    logPool,
-    cipherPool
 };
